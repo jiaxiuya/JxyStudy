8016d9fa7d43f8fbb018a48be1dfe5b320f35772
jiaxiuya <jiaxiuya@163.com>
2016-11-28 17:55:43 +0800

    kafka API

---
+++ src/main/java/com/jxy/kafka/consumer/ConsumerCommitSyncTest.java
@@ -0,0 +1,71 @@
+package com.jxy.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.*;
+
+/**
+ * <类描述>
+ * <功能详细描述>
+ *
+ * @author 贾秀亚
+ * @version [版本号, 2016/11/28 17:21]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class ConsumerCommitSyncTest {
+
+    /**
+     * 消费，并手动提交偏移量
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "false");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+        final int minBatchSize = 200;
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+        // 手动提交偏移量
+        /*while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                buffer.add(record);
+            }
+            if (buffer.size() >= minBatchSize) {
+                // 将读取的数据插入到数据库
+                //insertIntoDb(buffer);
+                consumer.commitSync();
+                buffer.clear();
+            }
+        }*/
+
+        // 手动提交具体的分片的具体偏移量
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Long.MAX_VALUE);
+                for (TopicPartition partition : records.partitions()) {
+                    List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
+                    for (ConsumerRecord<String, String> record : partitionRecords) {
+                        System.out.println(record.offset() + ": " + record.value());
+                    }
+                    long lastOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
+                    consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(lastOffset + 1)));
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+}
---
+++ src/main/java/com/jxy/kafka/consumer/ConsumerTest.java
@@ -0,0 +1,50 @@
+package com.jxy.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * <类描述>
+ * <功能详细描述>
+ *
+ * @author 贾秀亚
+ * @version [版本号, 2016/11/28 16:00]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class ConsumerTest {
+
+    /**
+     * 自动消费、提交偏移量
+     * @param args
+     */
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "192.168.1.112:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+
+        // 消费者可以指定订阅的分区，和直接订阅肢体不能混用
+        /*String topic = "foo";
+        TopicPartition partition0 = new TopicPartition(topic, 0);
+        TopicPartition partition1 = new TopicPartition(topic, 1);
+        consumer.assign(Arrays.asList(partition0, partition1));*/
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+        }
+    }
+}
