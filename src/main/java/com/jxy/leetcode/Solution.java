f8b9871f58d5c7fe398d85486073784330f652dc
xiuya.jxy <xiuya.jxy@alibaba-inc.com>
2020-09-12 01:56:12 +0800

    123

---
+++ src/main/java/com/jxy/leetcode/Solution.java
@@ -0,0 +1,12 @@
+package com.jxy.leetcode;
+
+class Solution {
+    public int reverse(int x) {
+        System.out.println(Integer.MAX_VALUE);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
